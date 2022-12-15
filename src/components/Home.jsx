d794e3558e815a3b51bf3231aa92b1863557c813
NwinNwin <ndnguyen2003@gmail.com>
2022-12-15 12:35:00 -0800

    working on homepage

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Home () {
+    return (
+        <>
+            <div className="home-div">
+                <h1 className="title-text">Quizzical</h1>
+                <p>just like Quizlet but better!</p>
+                <button className="home-btn">
+                    <h3>Start Quiz</h3>
+                </button>
+            </div>
+        </>
+    )
+}
