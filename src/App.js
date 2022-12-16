ff0aa4c0e3f2a27a234fdb38722d2040fdef598b
NwinNwin <ndnguyen2003@gmail.com>
2022-12-15 21:05:57 -0800

    complete home page, add ability to move to quiz

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from "react"
 import './App.css';
 import Home from './components/Home';
+import Quiz from "./components/Quiz";
+import blob2 from "./images/blob2.png"
+import blobs from "./images/blobs.png"
 
 function App() {
+  const [start, setStart] = useState(false)
+
+  const handleStart = () => {
+    setStart(!start)
+  }
+
   return (
     <div className="App">
-      <Home />
+       <img className="home-img2" src={blob2} alt="" />
+        <img className="home-img1" src={blobs} alt="" />
+      {!start ? 
+      <Home handleStart = {handleStart}/> 
+      : 
+      <Quiz/>
+      }
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,22 @@
 import React from "react"
+import blob2 from "../images/blob2.png"
+import blobs from "../images/blobs.png"
 
-export default function Home () {
+export default function Home (props) {
     return (
         <>
+       
             <div className="home-div">
-                <h1 className="title-text">Quizzical</h1>
-                <p>just like Quizlet but better!</p>
-                <button className="home-btn">
-                    <h3>Start Quiz</h3>
-                </button>
+                <div className="main-title">
+                    <h1 className="title-text">Quizzical</h1>
+                    <button onClick={props.handleStart} className="home-btn">
+                        <h3>Start Quiz</h3>
+                    </button>
+                </div>
+                
+               
             </div>
+            
         </>
     )
 }
