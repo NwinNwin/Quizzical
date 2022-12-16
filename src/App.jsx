a129fcbff51ed400f0991d26e77513673c4fc713
NwinNwin <ndnguyen2003@gmail.com>
2022-12-15 21:49:05 -0800

    fetch OpenTriviaDB & get 4 random Qs

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Quiz from "./components/Quiz";
 import blob2 from "./images/blob2.png"
 import blobs from "./images/blobs.png"
 
+
 function App() {
   const [start, setStart] = useState(false)
 
@@ -14,8 +15,9 @@ function App() {
 
   return (
     <div className="App">
-       <img className="home-img2" src={blob2} alt="" />
-        <img className="home-img1" src={blobs} alt="" />
+      <img className="home-img2" src={blob2} alt="" />
+      <img className="home-img1" src={blobs} alt="" />
+
       {!start ? 
       <Home handleStart = {handleStart}/> 
       : 
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
 
 export default function Quiz() {
+
+    const opentdbAPI = "https://opentdb.com/api.php?amount=50&category=18&difficulty=medium&type=multiple";
+
+    const [quizData, setQuizData] = useState([]);
+    const [currentQuiz, setCurrentQuiz] = useState([]);
+
+
+
+    async function getQuizData() {
+        const result = await axios.get(opentdbAPI);
+        setQuizData(result.data.results) 
+
+    }
+
+    function getRandom1_50() {
+        return Math.floor(Math.random() * 50);
+    }
+
+    function getFourQs () {
+        let tempList = []
+        for (let i = 0; i < 4; i++){
+            const indexForQuiz = getRandom1_50()
+            tempList.push(quizData[indexForQuiz])
+        }
+        setCurrentQuiz(prev => tempList.map(ele => ele))
+        console.log(currentQuiz)
+    }
+
+    function handleNewQuiz() {
+        getFourQs()
+    }
+
+    useEffect(() => {
+        getQuizData()
+        
+    }, [])
+
     return (
-        <h1>hi</h1>
+        <div className="quiz-div">
+            <button onClick={handleNewQuiz}>new Quiz</button>
+        </div>
+
     )
 }
